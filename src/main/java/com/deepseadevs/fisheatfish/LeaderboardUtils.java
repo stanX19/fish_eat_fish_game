726373056ac36b5f33ca49b1f6fc9063b56434bf
StanX19 <tanshanchien@gmail.com>
2024-11-18 09:33:13 +0800

    added top users in leaderboard page

--- src/main/java/com/deepseadevs/fisheatfish/LeaderboardUtils.java
+++ src/main/java/com/deepseadevs/fisheatfish/LeaderboardUtils.java
@@ -3,11 +3,13 @@ package com.deepseadevs.fisheatfish;
 import java.util.Collection;
 
 public class LeaderboardUtils {
-    public static void getTopUsers(int n) {
+    public static Collection<UserData> getTopUsers(int n) {
         Collection<UserData> allData = DatabaseManager.getInstance().getAllUserData();
         // TODO:
         //  Complete this function
         //  this function will be used by leaderboard
         //  return top n users based on high score
+        //  remember to change the return value
+        return allData;
     }
 }
