7138d2a1157185be0e2906d3983430e6b5ddffa9
HZ Tan <zhe.ht0623@gmail.com>
2024-11-22 13:34:16 +0800

    overwrited render method to drawFishRight and drawFishLeft added drawFishLeft method that shows fishes facing the left side

--- src/main/java/com/deepseadevs/fisheatfish/BaseFish.java
+++ src/main/java/com/deepseadevs/fisheatfish/BaseFish.java
@@ -11,7 +11,7 @@ public class BaseFish extends GameObject {
 
     // TODO
     //  Overwrite render method to draw a fish
-    public void render(GraphicsContext gc) {
+    public void drawFishRight(GraphicsContext gc) {
         // Set the fill color for the fish's body
         gc.setFill(Color.ORANGE);
 
@@ -30,6 +30,28 @@ public class BaseFish extends GameObject {
         gc.setFill(Color.BLACK);
 
         // Draw the fish's eye as a circle, positioned on the left side of the body
-        gc.fillOval(getX() + getWidth() / 1.5, getY() + getHeight() / 3, getWidth() / 7, getHeight() / 7);
+        gc.fillOval(getX() + getWidth() / 1.5, getY() + getHeight() / 3, getWidth() / 6, getHeight() / 7);
+    }
+
+    public void render(GraphicsContext gc) {
+        // Set the fill color for the fish's body
+        gc.setFill(Color.SILVER);
+
+        // Draw the fish's body as an oval, facing left
+        gc.fillOval(getX(), getY(), getWidth(), getHeight());
+
+        // Set the fill color for the fish's tail
+        gc.setFill(Color.RED);
+
+        // Draw the fish's tail as a triangle, facing left
+        double[] tailX = {getX() + getWidth(), getX() + getWidth() * 1.2, getX() + getWidth()};
+        double[] tailY = {getY() + getHeight() / 2, getY() + getHeight() * 0.8, getY() + getHeight()};
+        gc.fillPolygon(tailX, tailY, 3);
+
+        // Set the fill color for the fish's eye
+        gc.setFill(Color.BLACK);
+
+        // Draw the fish's eye as a circle, positioned on the right side of the body
+        gc.fillOval(getX() + getWidth() / 4.5, getY() + getHeight() / 3, getWidth() / 6, getHeight() / 7);
     }
 }
