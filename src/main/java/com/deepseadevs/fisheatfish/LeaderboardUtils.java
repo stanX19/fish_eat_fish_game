97b3d411980851df518a4c29df01b7789279d5e0
deryung <cdyung720@gmail.com>
2024-11-26 23:59:33 +0800

    Completed leaderboardUtils

--- src/main/java/com/deepseadevs/fisheatfish/LeaderboardUtils.java
+++ src/main/java/com/deepseadevs/fisheatfish/LeaderboardUtils.java
@@ -1,15 +1,26 @@
 package com.deepseadevs.fisheatfish;
-
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Collection;
+import java.util.Collections;
 
 public class LeaderboardUtils {
     public static Collection<UserData> getTopUsers(int n) {
         Collection<UserData> allData = DatabaseManager.getInstance().getAllUserData();
-        // TODO:
-        //  Complete this function
-        //  this function will be used by leaderboard
-        //  return top n users based on high score
-        //  remember to change the return value
-        return allData;
+
+
+        if (allData == null || allData.isEmpty() || n <= 0) {
+            // Return an empty collection if no data is available or invalid input
+            return Collections.emptyList();
+        }
+
+        // Create a list to sort the data (since Collection doesn't support direct sorting)
+        ArrayList<UserData> userList = new ArrayList<>(allData);
+
+        // Sort the list in descending order of high scores
+        userList.sort(Comparator.comparingLong(UserData::getHighScore).reversed());
+
+        // Return the top n users (or all users if n exceeds the available data)
+        return userList.subList(0, Math.min(n, userList.size()));
     }
 }
