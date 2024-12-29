1739026fc02fca32d447192bc44913e522f7f8e2
HZ Tan <zhe.ht0623@gmail.com>
2024-12-29 23:16:30 +0800

    made adjustment on mediumfish and smallplayerfish bound dimension

--- src/main/java/com/deepseadevs/fisheatfish/game/fish/SmallPlayerFish.java
+++ src/main/java/com/deepseadevs/fisheatfish/game/fish/SmallPlayerFish.java
@@ -7,7 +7,7 @@ import javafx.scene.canvas.GraphicsContext;
 public class SmallPlayerFish extends BasePlayerFish {
     public SmallPlayerFish() {
         super();
-        leftFishAnimation = new Animation("file:src/main/assets/sprites/orenleft.png", 1, new Bound(0,14.4,64,60));
-        rightFishAnimation = new Animation("file:src/main/assets/sprites/orenright.png", 1, new Bound(0,14.4,64,60));
+        leftFishAnimation = new Animation("file:src/main/assets/sprites/orenleft.png", 0.75, new Bound(0,12,63,50));
+        rightFishAnimation = new Animation("file:src/main/assets/sprites/orenright.png", 0.75, new Bound(0,12,63,50));
     }
 }
