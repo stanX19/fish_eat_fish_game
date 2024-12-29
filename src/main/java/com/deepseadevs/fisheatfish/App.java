bdce807ef5e51146bbfd750b532420908e096e1c
HZ Tan <zhe.ht0623@gmail.com>
2024-12-29 17:41:29 +0800

    added few designs to fishes, and modified the bound dimensions for different fishes

--- src/main/java/com/deepseadevs/fisheatfish/App.java
+++ src/main/java/com/deepseadevs/fisheatfish/App.java
@@ -1,7 +1,6 @@
 package com.deepseadevs.fisheatfish;
 
 import javafx.application.Application;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
--- src/main/java/com/deepseadevs/fisheatfish/game/fish/LargeFish.java
+++ src/main/java/com/deepseadevs/fisheatfish/game/fish/LargeFish.java
@@ -8,7 +8,7 @@ import com.deepseadevs.fisheatfish.game.Bound;
 public class LargeFish extends BaseFish {
     public LargeFish() {
         super(200, 120, 80);
-        leftFishAnimation = new Animation("file:src/main/assets/sprites/pacmanleft.png", 0.1, new Bound(8,14,111,112));
-        rightFishAnimation = new Animation("file:src/main/assets/sprites/pacmanright.png", 0.1, new Bound(8,14,111,112));
+        leftFishAnimation = new Animation("file:src/main/assets/sprites/salmonleft.png", 0.75, new Bound(1,11,52,31));
+        rightFishAnimation = new Animation("file:src/main/assets/sprites/salmonright.png", 0.75, new Bound(12,11,63,31));
     }
 }
